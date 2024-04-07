0fad173f3fda265588c44ae27ab03497dd330a6a
smw989 <145391441+smw989@users.noreply.github.com>
2024-04-07 02:02:45 -0600

    as

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -22,9 +22,8 @@ const LoginForm = ({ switchToSignup }) => {
       });
       const data = await response.json();
       alert(data.message); // Display the server response
-      
+      // Redirect to the product page after successful login
       if (response.status === 200) {
-        localStorage.setItem("isLoggedIn", "true"); // Set isLoggedIn on successful login
         navigate('/products'); // Redirect to the product page
       }
     } catch (error) {
@@ -32,7 +31,6 @@ const LoginForm = ({ switchToSignup }) => {
       alert('Error logging in. Please try again.'); // Display an error message if login fails
     }
   };
-  
 
   return (
     <div>
---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,10 @@
+// ProtectedRoute.js
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children }) => {
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+  return isLoggedIn ? children : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
